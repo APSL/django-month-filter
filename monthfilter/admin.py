77c86b7852736f558791ced39cee4a2803b57bf7
Francesc Arpi <francesc.arpi@gmail.com>
2015-08-02 19:47:14 +0200

    Fixed selected item bug.

--- monthfilter/admin.py
+++ monthfilter/admin.py
@@ -12,16 +12,25 @@ from django.utils.translation import ugettext_lazy as _
 class MonthListFilter(admin.DateFieldListFilter):
     title = _('Month')
 
+    def __init__(self, field, request, params, model, model_admin, field_path):
+        self.lookup_kwarg = '%s__month' % field_path
+        self.lookup_val = request.GET.get(self.lookup_kwarg)
+        super(MonthListFilter, self).__init__(
+            field, request, params, model, model_admin, field_path)
+
+    def expected_parameters(self):
+        return [self.lookup_kwarg]
+
     def choices(self, cl):
         yield {
-            'selected': None,
+            'selected': self.lookup_val is None,
             'query_string': cl.get_query_string({}, [self.field.name]),
             'display': _('All'),
         }
 
         for month in range(1, 13):
             yield {
-                'selected': None,
+                'selected': self.lookup_val == str(month),
                 'query_string': cl.get_query_string(
                     {'{}__month'.format(self.field.name): month},
                     [self.field.name]),
